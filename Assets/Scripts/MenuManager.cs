df74a8f3c7722e2a5e2ab341d7175a896f690fd0
BlackCatSCA <canosalexander@gmail.com>
2025-05-05 20:32:21 -0500

    Implemetacion de Input field
    se configura el campo de entrada de nombre del jugador

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,24 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEditor;
 
 public class MenuManager : MonoBehaviour
 {
-    public GameObject startButton;
+    public InputField inputField;
+    public string namePlayer;
     // Start is called before the first frame update
-    /*void Start()
+    void Start()
     {
-        
+    
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }*/
+    }
+
+    public void ReadInput()
+    {
+        namePlayer = inputField.text;
+        Debug.Log("El usuario ingreso: " +  namePlayer);
+        
+    }
 
     public void OnClicStart()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+                EditorApplication.ExitPlaymode();
+        #else
+        Aplication.Quit()
+        #endif
     }
 }
